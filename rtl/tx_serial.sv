9c9758f004dd7357ce49b75e294acca7bb6c7f00
Julisa Verdejo <julisa.verdejo@gmail.com>
2025-05-14 12:29:28 +0200

    feat: count 2 clk cycles, bug: ena is not activate when cnt = 9

--- rtl/tx_serial.sv
+++ rtl/tx_serial.sv
@@ -35,25 +35,29 @@ module tx_serial (
       rdisp_q <= 'd0;
       shift_reg_q <= 'd0;
       cnt_q <= 'd9;
+      ena <= 1'b1;
       tick <= 'b0;
     end else begin 
       if (ena) begin 
         rdisp_q <= rdisp_d;
         shift_reg_q <= shift_reg_d;
         cnt_q <= 'b0;
+        ena <= 'b0;
       end else begin
+        tick <= tick + 1;
         if (tick) begin
-          shift_reg_q <= shift_reg_q >> 1;
-          cnt_q <= cnt_q + 1;
-          tick <= tick + 1;
-        end else begin
-          tick <= tick + 1;
-        end
+          if (cnt_q == 9) begin
+            ena <= 1'b1;
+          end else begin
+            shift_reg_q <= shift_reg_q >> 1;
+            cnt_q <= cnt_q + 1;
+          end
+        end 
       end
     end
   end
   
-  assign ena = (cnt_q == 'd9);
+  //assign ena = (cnt_q == 'd9);
   assign data_o = shift_reg_q[0];
   assign ena_o = ena;
   
