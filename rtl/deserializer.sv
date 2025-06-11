a3042ee2c798b21851dc5b4f0d6692022dae3332
Julisa Verdejo <julisa.verdejo@gmail.com>
2025-06-11 17:54:12 +0200

    feat: filling empty register with inputdata and assigning this register to data

--- rtl/deserializer.sv
+++ rtl/deserializer.sv
@@ -11,10 +11,10 @@ module deserializer #(
 );
 
   //Internal signals
-  logic coderr, disperr;
+  logic coderr_d, disperr_d;
  
   //Signal declaration
-  logic [WIDTH-1:0] shift_reg_q, shift_reg_d;
+  logic [WIDTH-1:0] shift_reg_q, shift_reg_d, shift_reg, data;
   logic [3:0] cnt_bits;  
   logic eob;
   logic rdisp_q, rdisp_d;
@@ -22,41 +22,42 @@ module deserializer #(
   logic disperr_q;
 
   decode_8b10b encoder_int (
-    .datain(shift_reg_q), 
+    .datain(10'hae), 
     .dispin(rdisp_q), 
     .dataout(outputdata_o), 
     .dispout(rdisp_d), 
-    .code_err(coderr), 
-    .disp_err(disperr)
+    .code_err(coderr_d), 
+    .disp_err(disperr_d)
   );
   
 
   always_ff @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin 
       rdisp_q     <= 1'b0;
-      shift_reg_q <= 10'b1001111100;
-      // shift_reg_d <= 'd0;
-      cnt_bits    <= 'd9;
+      shift_reg_q <= 10'd0;
+      shift_reg   <= 10'h27c;
+      cnt_bits    <= 'd0;
       coderr_q    <= 1'b0;
       disperr_q   <= 1'b0;
     end else begin 
       if (eob) begin 
         rdisp_q   <= rdisp_d;
-        // shift_reg_q <= shift_reg_d;
         shift_reg_q <= shift_reg_q;
+        shift_reg   <= shift_reg_q;
         cnt_bits  <= 'b0;
-        coderr_q  <= coderr;
-        disperr_q <= disperr;
+        coderr_q  <= coderr_d;
+        disperr_q <= disperr_d;
       end else begin 
-        // shift_reg_d <= {shift_reg_d[WIDTH-2:0], inputdata_i};
+        shift_reg   <= shift_reg;
         shift_reg_q <= {shift_reg_q[WIDTH-2:0], inputdata_i};
         cnt_bits  <= cnt_bits + 1;
       end
     end
   end
 
-  assign eob = (cnt_bits == 'd9);
+  assign eob = (cnt_bits == 'd11);
   assign eob_o = eob;
+  assign data = eob ? shift_reg_q: shift_reg;
   assign code_err_o = coderr_q;
   assign disp_err_o = disperr_q;
 
