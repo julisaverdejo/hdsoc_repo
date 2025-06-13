a52f2b691e539bc8501147b63f20038f0cf3db61
Julisa Verdejo <julisa.verdejo@gmail.com>
2025-06-13 16:32:57 +0200

    feat: add logic for start_read

--- rtl/wb_serializer.sv
+++ rtl/wb_serializer.sv
@@ -15,10 +15,10 @@
 `include "wb_serializer_pkg.sv"
 
 module wb_serializer (
-    input  logic  CLK_NEWFREQ_I, 
+  input  logic  CLK_NEWFREQ_I, 
 	input  logic  RST_NEWFREQ_I,
 	input  logic  INPUTDATA_I,
-    output logic  data_o,
+  output logic  data_o,
 	//output logic  ena_o,
 
     // WISHBONE BUS INTERFACE
@@ -28,7 +28,7 @@ module wb_serializer (
 	input  logic [31:0] ADR_I,  // address
 	input  logic [31:0] DAT_I,  // data input  
 	input  logic        STB_I,  // strobe      
-    input  logic        WE_I,   // write enable
+  input  logic        WE_I,   // write enable
 	output logic        ACK_O,  // acknowledge
 	output logic        ERR_O,  // error
 	output logic [31:0] DAT_O  // data output
@@ -39,15 +39,14 @@ module wb_serializer (
 
   //========================= Internal signals ========================= 
   // Signals for start with WB_FREQ
-  logic start, aux_start_d, aux_start_q;
+  logic start_write, aux_start_write_d, aux_start_write_q;
   //logic ena;
-  logic start_read;
   logic [8:0] wrd8b_data_q, wrd8b_data_in;
   logic [8:0] outputdata;
   logic eob, err;
 
-  // Signals for start_new with CLK_NEWFREQ
-  logic reg_aux1_start, reg_aux2_start, reg_aux3_start, start_new;
+  // Signals for start_new_write with CLK_NEWFREQ
+  logic reg_aux1_start, reg_aux2_start, reg_aux3_start, start_new_write;
 
   // Signals for end of transaction with CLK_NEWFREQ
   logic eot_stretch, eot_aux1, eot_aux2, eot_aux3;
@@ -69,30 +68,56 @@ module wb_serializer (
     
   //Signal declaration
   logic [31:0] data;
-
+  logic eob_aux1, eob_aux2, eob_aux3, EOB_I;
+  logic aux_start_read_d, aux1_start_read_q, aux2_start_read_q;
+  logic start_read, start_new_read;  
 
 // WISHBONE BUS
-
+  always_ff @(posedge CLK_I, posedge RST_I) begin
+	  if(RST_I) begin
+	    eob_aux1 <= 1'b0;
+	    eob_aux2 <= 1'b0;
+      EOB      <= 1'b0;
+	  end else begin
+      eob_aux1 <= eob;
+      eob_aux2 <= eob_aux1;
+      EOB      <= ~eob_aux2;
+	  end
+  end
 
 // start read to know when the conversion is finished and pass the 8-bit DAT_I (WB signal)
-  assign start_read = (CYC_I && STB_I && ~WE_I && (ADR_I = ADR_READ) && EOB);
+  assign aux_start_read_d = (CYC_I & STB_I & ~WE_I & (ADR_I = ADR_READ) & EOB);
+
+  always_ff @(posedge CLK_I, posedge RST_I) begin
+    if (RST_I) begin
+      aux1_start_read_q <= 1'b0;
+    end else begin
+      aux2_start_read_q <= aux_start_read_d;
+    end
+  end
+
+  assign start_read = ~aux2_start_read_q & aux_start_read_d;
+
+  always_ff @(posedge CLK_I, posedge RST_I) begin
+    if (RST_I) begin
+      start_new_read <= 1'b0;
+    end else begin
+      start_new_read <= start_read;
+    end
+  end  
 
 // Add a flip-flop to pass from one clk frequency to another (WB Frequency)
   always_ff @(posedge CLK_I, posedge RST_I) begin
 	if (RST_I) begin
 	  wrd8b_data_in <= 9'b0; 
 	end else begin
-	  if (start_read) begin
+	  if (start_new_read) begin
 	    wrd8b_data_q <= outputdata;
-        wrd8b_data_in <= wrd8b_data_q;
+      rd8b_data_in <= wrd8b_data_q;
 	  end else begin
 	    wrd8b_data_in <= wrd8b_data_in;
 	  end
   end
-
-
-
-
 
 
 	// READ: SLAVE ---> MASTER
@@ -125,10 +150,10 @@ module wb_serializer (
     if (RST_I) begin
       //start <= 'b0;
 	  data  <= 'b0;
-	  aux_start_q <= 1'b0;
+	  aux_start_write_q <= 1'b0;
     end else begin
       //start <= 'b0;
-      aux_start_q <= aux_start_d;
+      aux_start_write_q <= aux_start_write_d;
 	  if (WE_I && STB_I) begin
 	    case (ADR_I[ADDR_SIZE-1:0])	
 
@@ -145,57 +170,57 @@ module wb_serializer (
   end
 
   //assign ena_o = ena;
-  assign aux_start_d = CYC_I & STB_I & WE_I & (ADR_I == ADR_WRITE);
-  assign start = aux_start_q;
+  assign aux_start_write_d = CYC_I & STB_I & WE_I & (ADR_I == ADR_WRITE);
+  assign start_write = aux_start_write_q;
 
   //========================= CROSSING CLOCK DOMAIN ========================= 
 
   always_ff @(posedge CLK_NEWFREQ_I, posedge RST_NEWFREQ_I) begin
     if (RST_NEWFREQ_I) begin
-      reg_aux1_start <= 'b0;
-      reg_aux2_start <= 'b0;	
-	  start_new     <= 'b0;
+      reg_aux1_start  <= 'b0;
+      reg_aux2_start  <= 'b0;	
+	    start_new_write <= 'b0;
     end else begin
-      reg_aux1_start <= start;
+      reg_aux1_start <= start_write;
       reg_aux2_start <= reg_aux1_start;
-	  if (reg_aux2_start == 0 && reg_aux1_start == 1) begin
-        start_new <= 1'b1;
-	  end else begin
-	    start_new <= 1'b0;
-	  end
+	    if (reg_aux2_start == 0 && reg_aux1_start == 1) begin
+        start_new_write <= 1'b1;
+	    end else begin
+	      start_new_write <= 1'b0;
+	    end
     end
   end
 
   serializer_in mod_serialin (
     .clk_i(CLK_NEWFREQ_I),
     .rst_i(RST_NEWFREQ_I),
-    .start_i(start_new),
+    .start_i(start_new_write),
     .data_i(data),       
     .data_o(data_o),
 	//.ena_o(ena),
-	.eot_o(eot)
+	  .eot_o(eot)
   );
 
   always_ff @(posedge CLK_NEWFREQ_I, posedge RST_NEWFREQ_I) begin
-	if(RST_NEWFREQ_I) begin
-	  eot_stretch <= 1'b0;
-	end else if (eot) begin
+	  if(RST_NEWFREQ_I) begin
+	    eot_stretch <= 1'b0;
+	  end else if (eot) begin
       eot_stretch <= ~eot_stretch;
-	end else begin
-	  eot_stretch <= eot_stretch;
-	end
+	  end else begin
+	    eot_stretch <= eot_stretch;
+	  end
   end
 
   always_ff @(posedge CLK_I, posedge RST_I) begin
-	if(RST_I) begin
-	  eot_aux1 <= 1'b0;
-	  eot_aux2 <= 1'b0;
-	  eot_aux3 <= 1'b0;
-	end else begin
-	  eot_aux1 <= eot_stretch;
-	  eot_aux2 <= eot_aux1;
-	  eot_aux3 <= eot_aux2;
-	end
+	  if(RST_I) begin
+	    eot_aux1 <= 1'b0;
+	    eot_aux2 <= 1'b0;
+	    eot_aux3 <= 1'b0;
+	  end else begin
+	    eot_aux1 <= eot_stretch;
+	    eot_aux2 <= eot_aux1;
+	    eot_aux3 <= eot_aux2;
+	  end
   end
   assign eot_new = eot_aux3 ^ eot_aux2;
 endmodule
